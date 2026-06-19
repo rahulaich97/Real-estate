bccd5bacb4075eb034e5791fed57216701d6f209
rahulaich97 <rahulaich97@gmail.com>
2026-06-19 16:12:52 +0530

    fixminifier

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css:{
-    transformers: "postcss"
+  plugins: [react(), tailwindcss()],
+  build:{
+    cssMinify: 'esbuild'
   }
 })
