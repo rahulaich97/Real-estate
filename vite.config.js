c66c3a408c2c72565556967ed64174516ea36d61
rahulaich97 <rahulaich97@gmail.com>
2026-06-19 16:20:42 +0530

    migrate to tailwind v3

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build:{
-    cssMinify: 'esbuild'
-  }
+  plugins: [react()],
 })
